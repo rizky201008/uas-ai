865eddeecb4b151e80b37aac73a64390e2f7666a
farrela46 <gpay46@gmail.com>
2024-06-13 16:19:22 +0700

    Feat: add FE

---
+++ fe-vue-mammals/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar navbar-expand-lg " style="background-color: #2E005E;">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#">What's Mammals ?</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Whats Mammals ?</a>
+                    </li>
+                </ul>
+            </div> -->
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
+
+<style>
+/* Add any custom styles here */
+</style>
---
+++ fe-vue-mammals/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="height: 100vh; background-color: #421983;">
+    <Navbar />
+    <div class="container text-center d-flex align-items-center">
+      <div class="row mt-5">
+        <div class="col-md-6 d-flex align-items-center">
+          <div class="wrap">
+            <h1 class="display-4 text-white">Mamalia apa Hayo?</h1>
+            <p class="lead text-white">A quick website to identify what's mammal that you see today.
+            </p>
+            <form class="d-flex justify-content-center align-items-center">
+              <input type="file" class="form-control me-2" placeholder="Insert email address" aria-label="Email">
+              <button class="btn btn-success" type="submit">Cari</button>
+            </form>
+          </div>
+        </div>
+        <div class="col-md-6 d-flex align-items-center justify-content-center">
+          <div class=" my-5">
+            <img src="../assets/mammals.png" style="width: 100%;">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+form input {
+  width: 250px;
+}
+
+form button {
+  width: 100px;
+}
+</style>
